a78a23b7b808a28e14151a497bba44b71d0b4ff6
Rhett Carver <rcarver3@gatech.edu>
2024-01-25 14:31:41 -0500

    kind of got something working, still needs a lot of work. there's a list and it has dropdowns but it crashes all the time and its kinda ugly tbh idrk why

--- app/src/main/java/com/example/collegescheduler/MainActivity.java
+++ app/src/main/java/com/example/collegescheduler/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.collegescheduler;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -16,8 +19,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.collegescheduler.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class MainActivity extends AppCompatActivity {
+    List<String> groupList;
+    List<String> childList;
+    Map<String, List<String>> mobileCollection;
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -25,5 +37,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createGroupList();
+        createCollection();
+        expandableListView = findViewById(R.id.elvMobiles);
+        expandableListAdapter = new MyExpandableListAdapter(this, groupList, mobileCollection);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            int lastExpandedPosition = -1;
+            @Override
+            public void onGroupExpand(int i) {
+                if (lastExpandedPosition != -1 && i != lastExpandedPosition) {
+                    expandableListView.collapseGroup(lastExpandedPosition);
+                }
+                lastExpandedPosition = i;
+            }
+        });
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long id) {
+                String selected = expandableListAdapter.getChild(i, i1).toString();
+                Toast.makeText(getApplicationContext(), "Selected: " + selected, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void createCollection() {
+        String[] csInfo = {"Objects and Design", "12:30 - 1:45", "Pedro"};
+        String[] eceInfo = {"Power, Arch, and Conc", "9:30 - 10:45", "Brothers"};
+        String[] mathInfo = {"Diff Eq", "2:00 - 3:15", "Su"};
+        mobileCollection = new HashMap<String, List<String>>();
+        for (String group : groupList) {
+            if (group.equals("CS 2340")) {
+                loadChild(csInfo);
+            } else if (group.equals("ECE 3058")) {
+                loadChild(eceInfo);
+            } else {
+                loadChild(mathInfo);
+            }
+            mobileCollection.put(group, childList);
+        }
+    }
+
+    private void loadChild(String[] classes) {
+        childList = new ArrayList<>();
+        for (String cl : classes) {
+            childList.add(cl);
+        }
+    }
+
+    private void createGroupList() {
+        groupList = new ArrayList<>();
+        groupList.add("CS 2340");
+        groupList.add("ECE 3058");
+        groupList.add("MATH 2552");
     }
 }
---
+++ app/src/main/java/com/example/collegescheduler/MyExpandableListAdapter.java
@@ -0,0 +1,121 @@
+package com.example.collegescheduler;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.Map;
+
+public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private Map<String, List<String>> mobileCollection;
+    private List<String> groupList;
+
+    public MyExpandableListAdapter(Context context, List<String> groupList,
+                                   Map<String, List<String>> mobileCollection) {
+        this.context = context;
+        this.mobileCollection = mobileCollection;
+        this.groupList = groupList;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return mobileCollection.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return mobileCollection.get(groupList.get(i)).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return groupList.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return mobileCollection.get(groupList.get(i)).get(i1);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        String className = getGroup(i).toString();
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.group_item, null);
+        }
+        TextView item = view.findViewById(R.id.mobile);
+        item.setTypeface(null, Typeface.BOLD);
+        item.setText(className);
+        return view;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        String model = getChild(i, i1).toString();
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.child_item, null);
+        }
+        TextView item = view.findViewById(R.id.model);
+        ImageView delete = view.findViewById(R.id.delete);
+        item.setText(model);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Are you sure you want to remove this?");
+                builder.setCancelable(true);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        List<String> child = mobileCollection.get(groupList.get(i));
+                        child.remove(i1);
+                        notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
