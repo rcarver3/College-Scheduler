fc1c38f04e3c2a51f24bec4b418516280835d1f1
Rhett Carver <rcarver3@gatech.edu>
2024-02-05 22:13:00 -0500

    instructor added

--- app/src/main/java/com/example/collegescheduler/RecyclerViewInterface.java
+++ app/src/main/java/com/example/collegescheduler/RecyclerViewInterface.java
@@ -2,5 +2,5 @@ package com.example.collegescheduler;
 
 public interface RecyclerViewInterface {
     void onItemClick(int position);
-    void onItemLongClick(int position);
+    void onItemLongClick(String model, int position);
 }
